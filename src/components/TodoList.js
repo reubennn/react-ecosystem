c1d1b359d66febcab338cb3159b6905583ebf322
Reuben Smith <reuben-smith@hotmail.com>
2020-09-24 16:12:33 +0930

    Implements the React-Redux application by adding Redux to the React Components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import TodoListItem from "./TodoListItem"
 import NewTodoForm from "./NewTodoForm";
+import { removeTodo } from "../actions/todoActions";
 import "./TodoList.css";
 
 // todos has default property to avoid getting an error
-const TodoList = ({ todos = [{text: "Hello"}]}) => (
+const TodoList = ({ todos = [], onRemovePressed }) => (
     <div className="list-wrapper">
         <NewTodoForm />
-        {todos.map((todo) => < TodoListItem todo={todo} />)}
+        {todos.map((todo) => < TodoListItem todo={todo} onRemovePressed={onRemovePressed} />)}
     </div>
 );
 
-export default TodoList;
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./TodoListItem.css";
 
-const TodoListItem = ({ todo }) => (
+const TodoListItem = ({ todo, onRemovePressed }) => (
     <div className="todo-item-container card">
         <h3 className="card-header"></h3>
         <div className="card-body text-center">
             <h4 className="card-title">{todo.text}</h4>
-            <p class="card-text">Todo information here...</p>
             <div className="button-container">
                 <button className="completed-button btn btn-primary">Mark As Completed</button>
-                <button className="remove-button btn btn-danger">Remove</button>
+                <button
+                    className="remove-button btn btn-danger"
+                    onClick={() => onRemovePressed(todo.text)}
+                >
+                    Remove
+                </button>
             </div>
         </div>
     </div>
