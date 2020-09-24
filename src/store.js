99394b51532a399c1dc7e86e9bd4dae24dd09775
Reuben Smith <reuben-smith@hotmail.com>
2020-09-24 16:28:44 +0930

    Persists the Redux store using redux-persist
    * State information is stored

--- src/store.js
+++ src/store.js
@@ -1,11 +1,29 @@
 import {createStore, combineReducers} from "redux";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import {todos} from "./reducers/todoReducer";
 
 const reducers = {
     todos,
 };
 
+const persistConfig = {
+    key: "root",
+    storage, // Defaults to local storage on the web
+    stateReconciler: autoMergeLevel2, /* Tells Redux persist how to reconcile initial
+                                       * and stored states of the Application
+                                       * - as in how deep should it go
+                                       */
+}
+
 // Put Reducers into a form which we can pass to the createStore function
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+/**
+ * persistConfig => Object which tells Redux Persist how to save and
+ * where to store the applications data
+ */
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
