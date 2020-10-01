7d702beb4cdb8b484199974ced85629887a915c6
Reuben Smith <reuben-smith@hotmail.com>
2020-10-01 16:52:20 +0930

    Adds tests for Redux Selectors
    * Tests getIncompleteTodos selector
    * Tests getCompletedTodos selector

---
+++ src/tests/todoSelectors.test.js
@@ -0,0 +1,50 @@
+import { expect } from "chai";
+import { getCompletedTodos, getIncompleteTodos } from "../selectors/todoSelectors";
+
+describe("REDUX SELECTORS:", () => {
+    it("getCompletedTodos: Returns only completed to-dos", () => {
+        const fakeTodos = [{
+            text: "Say Hello",
+            isCompleted: true,
+        }, {
+            text: "Say Goodbye",
+            isCompleted: false,
+        }, {
+            text: "Climb Mount Everest",
+            isCompleted: false,
+        }];
+
+        const expected = [{
+            text: "Say Hello",
+            isCompleted: true,
+        }];
+
+        const actual = getCompletedTodos.resultFunc(fakeTodos);
+
+        expect(actual).to.deep.equal(expected);
+    }),
+        it("getIncompleteTodos: Returns only incompleted to-dos", () => {
+            const fakeTodos = [{
+                text: "Say Hello",
+                isCompleted: true,
+            }, {
+                text: "Say Goodbye",
+                isCompleted: false,
+            }, {
+                text: "Climb Mount Everest",
+                isCompleted: false,
+            }];
+
+            const expected = [{
+                text: "Say Goodbye",
+                isCompleted: false,
+            }, {
+                text: "Climb Mount Everest",
+                isCompleted: false,
+            }];
+
+            const actual = getIncompleteTodos.resultFunc(fakeTodos);
+
+            expect(actual).to.deep.equal(expected);
+        });
+});
