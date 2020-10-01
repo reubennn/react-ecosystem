a0f603e0c66e28dea4bcf66442503fcd4529c1e8
Reuben Smith <reuben-smith@hotmail.com>
2020-10-01 15:29:03 +0930

    Adds testing file for testing Redux reducers from todoReducers.js
    * Adds testing dependencies:
        "@babel/register": "^7.11.5",
        "chai": "^4.2.0",
        "mocha": "^8.1.3",

---
+++ src/tests/todoReducers.test.js
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import { todos } from "../reducers/todoReducers";
+
+describe("To-dos Reducer", () => {
+    it("Adds a new to-do when CREATE_TODO action is received", () => {
+        const fakeTodo = {
+            text: "hello",
+            isCompleted: false
+        };
+        const fakeAction = {
+            type: "CREATE_TODO",
+            payload: {
+                todo: fakeTodo,
+            },
+        };
+        const originalState = {
+            isLoading: false,
+            data: [],
+        };
+
+        const expected = {
+            isLoading: false,
+            data: [fakeTodo],
+        };
+        const actual = todos(originalState, fakeAction);
+
+        expect(actual).to.deep.equal(expected);
+    });
+});
